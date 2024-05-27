15442afdb30ada13bdba67b2312c16fda8402d64
pawan1705 <pawan.sangare05@gmailcom>
2024-05-27 17:15:05 +0530

    first

---
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './PlayVideo.css';
+const PlayVideo = () => {
+  return (
+    <div>PlayVideo</div>
+  )
+}
+
+export default PlayVideo
---
+++ src/assets/assets.js
@@ -0,0 +1,91 @@
+import automobiles from './automobiles.png'
+import blogs from './blogs.png'
+import cast from './cast.png'
+import dislike from './dislike.png'
+import entertainment from './entertainment.png'
+import game_icon from './game_icon.jpeg'
+import history from './history.jpeg'
+import home from './home.png'
+import library from './library.png'
+import like from './like.png'
+import logo from './logo.png'
+import me from './me.png'
+import menu from './menu.png'
+import message from './message.png'
+import more from './more.png'
+import music from './music.png'
+import news from './news.png'
+import notification from './notification.jpeg'
+import save from './save.png'
+import search from './search.png'
+import share from './share.png';
+import show_more from './show_more.png';
+import sports from './sports.png';
+import subscription from './subscription.png'
+import tech from './tech.png'
+import upload from './upload.png'
+import video from './video.mp4'
+import voice_search from './voice_search.png'
+import maya from './maya.jpg'
+import modi from './modi.jpg'
+import kejri from './kejri.jpg'
+import thumbnail1 from './thumbnail1.png'
+import thumbnail2 from './thumbnail2.png'
+import thumbnail3 from './thumbnail3.png'
+import thumbnail4 from './thumbnail4.png'
+import thumbnail5 from './thumbnail5.png'
+import thumbnail6 from './thumbnail6.png'
+import thumbnail7 from './thumbnail7.png'
+import thumbnail8 from './thumbnail8.png'
+import youtube from './youtube.png'
+import insta from './insta2.png'
+import fb from './fb.png'
+import x from './x.png'
+
+export const assets = {
+    automobiles,
+    blogs,
+    cast,
+    dislike,
+    entertainment,
+    game_icon,
+    history,
+    home,
+    library,
+    like,
+    logo,
+    me,
+    menu,
+    message,
+    more,
+    music,
+    news,
+    notification,
+    save,
+    search,
+    share,
+    show_more,
+    sports,
+    subscription,
+    tech,
+    upload,
+    video,
+    voice_search,
+    maya,
+    modi,
+    kejri,
+    youtube,
+    insta,
+    fb,
+    x,
+    thumbnail1,
+    thumbnail2,
+    thumbnail3,
+    thumbnail4,
+    thumbnail5,
+    thumbnail6,
+    thumbnail7,
+    thumbnail8
+
+
+}
